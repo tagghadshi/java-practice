266e8e48add3dfef68ce369e7616ba2fc819d569
tagghadshi <tagghadshi@gmail.com>
2021-08-10 10:17:47 +0000

    VarArgs

--- src/com/tcs/entity/ArraysDemo.java
+++ src/com/tcs/entity/ArraysDemo.java
@@ -4,9 +4,23 @@ import java.util.Arrays;
 
 public class ArraysDemo {
 	public static void main(String[] args) {
+		EnchanceForLoop();
+		//demo();
+	}
+
+	public static void EnchanceForLoop() {
+		int[] numbers = { 3, 4, -1, 2, 6 };
+		for (int number : numbers) {
+			if (number % 2 == 0) {
+				System.out.println(number);
+			}
+		}
+	}
+
+	public static void demo() {
 		int[] numbers = new int[10];
-		numbers[0]=5;
-		numbers[4]=2;
+		numbers[0] = 5;
+		numbers[4] = 2;
 		Arrays.sort(numbers, 0, numbers.length);
 		for (int i = 0; i < numbers.length; i++) {
 			System.out.println(numbers[i]);

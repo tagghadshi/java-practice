7a4da210664b7a3e1a8b17b9211639f46081fe86
tagghadshi <tagghadshi@gmail.com>
2021-08-12 06:13:37 +0000

    Function Programming, Lambda Function

--- src/com/tcs/listdemo/ArraylistDemo.java
+++ src/com/tcs/listdemo/ArraylistDemo.java
@@ -4,11 +4,34 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class ArraylistDemo {
 	public static void main(String[] args) {
 		List<Integer> numbers = createList();
 		System.out.println(numbers);
+//		sortList(numbers);
+		filterList(numbers);
+
+	}
+
+	/**
+	 * Method to filter elements
+	 * 
+	 * @param numbers
+	 */
+	private static void filterList(List<Integer> numbers) { // Functional Programming
+		numbers.forEach((number) -> {
+			System.out.println(number % 2 == 0);
+		});
+		List<Integer> filtered = numbers.stream() // send elements one by one
+				.filter((number) -> number % 2 == 0) // Lambda Function
+				.collect(Collectors.toList());
+		System.out.println(filtered);
+	}
+
+	private static void sortList(List<Integer> numbers) {
 		numbers.sort(new Comparator<Integer>() {
 			@Override
 			public int compare(Integer o1, Integer o2) {
